c6538e76647175ffdda86e871a3b11b84d3e94d2
Vanderson <botelhojp@gmail.com>
2013-09-25 15:48:07 -0300

    assinatura pelo maven

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ echo 'CRONOS'
 mvn package
 
 #keytool -genkey -keystore cronosKeys -alias jdc
-jarsigner -keystore cronosKeys target/cronos-1.0.0-standalone.jar jdc
+#jarsigner -keystore cronosKeys target/cronos-1.0.0-standalone.jar jdc
 
 echo 'SCP'
-scp target/cronos-1.0.0-standalone.jar vanderson@hercules.ppgia.pucpr.br:/home2/doutorado/2013/vanderson/public_html/cronos
+scp target/cronos-1.0.0-standalone.jar vanderson@hercules.ppgia.pucpr.br:/home2/doutorado/2013/vanderson/public_html/cronos/cronos.jar
